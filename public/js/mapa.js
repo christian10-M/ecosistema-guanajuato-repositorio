34c50bcbda5ec6d1b15e3356325febd6f5ea966c
christian10-M <christiamomar2004@gmail.com>
2025-12-18 21:28:36 -0600

    Añadiendo capa minas - Refactorizando codigo en JS

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,28 +1,63 @@
+//Crea el mapa con centro en Guanajuato
 const map = L.map('map').setView([21.02, -101.25], 8);
 
+//Capa básica
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
+//CAPA AGUA
 const capaAgua = L.layerGroup().addTo(map);
+//CAPA MINAS
+const capaMinas = L.layerGroup().addTo(map);
 
-fetch('api/agua.php')
-  .then(r => r.json())
-  .then(data => {
-    data.forEach(punto => {
-      L.circleMarker([punto.lat, punto.lng], {
-        radius: 4,
-        color: '#0077cc',
-        fillOpacity: 0.7
-      })
-      .bindPopup(`
-        <strong>Municipio:</strong> ${punto.municipio}<br>
-        <strong>Estado:</strong> ${punto.ESTADO}
-      `)
-      .addTo(capaAgua);
+//Conexion con el backend
+//Refactorizamos código lol
+function cargarCapa({ url, capa, color, popup }) {
+    //Peticion HTTP al backend
+  fetch(url)
+  //Convierte la respuesta en JSON
+    .then(r => r.json())
+    .then(data => {
+    //Data es un arreglo de puntos
+      data.forEach(p => {
+    //Recorre cada fila Usa lat y lng
+        L.circleMarker([p.lat, p.lng], {
+        //Estilo del punto
+          radius: 4,
+          color,
+          fillOpacity: 0.7
+        })
+        //Popup informativo
+        .bindPopup(popup(p))
+        .addTo(capa);
+      });
     });
-  });
+}
 
+//Funciones especificas para cargar cada capa
+cargarCapa({
+  url: 'api/agua.php',
+  capa: capaAgua,
+  color: '#0077cc',
+  popup: p => `
+    <strong>Municipio:</strong> ${p.municipio}<br>
+    <strong>Estado:</strong> ${p.estado}
+  `
+});
+
+cargarCapa({
+  url: 'api/minas.php',
+  capa: capaMinas,
+  color: '#b30000',
+  popup: p => `
+    <strong>Municipio:</strong> ${p.municipio}<br>
+    <strong>Estado:</strong> ${p.estado}
+  `
+});
+
+//Caja para activar/desactivar capas
 L.control.layers(null, {
-  "Agua": capaAgua
+  "Agua": capaAgua,
+  "Minas": capaMinas
 }).addTo(map);
